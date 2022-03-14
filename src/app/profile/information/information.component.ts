8058f38e0b675221d6f512fda2097a70caa562e4
Sofia Carballo <sofiacarballo1990@gmail.com>
2022-03-13 23:19:26 -0300

    I added the doctor's profile with the corresponding information and the functionality that when clicking on the doctor takes me to his specific profile

---
+++ src/app/profile/information/information.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Doctor} from '../../models/doctor.model';
+
+@Component({
+  selector: 'app-information',
+  templateUrl: './information.component.html',
+  styleUrls: ['./information.component.scss'],
+})
+export class InformationComponent implements OnInit {
+  @Input() doctor!: Doctor;
+  isReadMore = true;
+  constructor() { }
+
+  ngOnInit() {}
+
+}
