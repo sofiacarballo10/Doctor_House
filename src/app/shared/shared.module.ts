fb32744adf5e76baa1e62fe03bfc0272c9c20956
Sofia Carballo <sofiacarballo1990@gmail.com>
2022-05-06 20:28:07 -0300

    add pipe img

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,10 +8,11 @@ import { IonicModule } from '@ionic/angular';
 import { BasicHeaderComponent } from './basic-header/basic-header.component';
 import { ItemComponent } from './item/item.component';
 import { SectionComponent } from './section/section.component';
+import { SafeStylesPipe } from './pipes/safe-styles.pipe';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, RouterModule],
   exports: [BasicHeaderComponent, ItemComponent, SectionComponent],
-  declarations: [BasicHeaderComponent, ItemComponent, SectionComponent],
+  declarations: [BasicHeaderComponent, ItemComponent, SectionComponent, SafeStylesPipe],
 })
 export class SharedPageModule {}
